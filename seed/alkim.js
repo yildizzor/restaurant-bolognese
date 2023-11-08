a3d952e05194a31de8ef49e24a477260aa6ffca6
Alkim <alkim-atanasov@outlook.com>
2023-11-08 21:51:33 +0100

    images_2 file and data added with json format

--- seed/alkim.js
+++ seed/alkim.js
@@ -1,50 +1,291 @@
-const dishes =[
-    {
-
-    title: "Bean",//example 1
-
-    ingredients: ["Bean", "Tomatoes", "Salt"], //example
-
-    cuisine: "Turkish",
-    dishType: "main_course",
-
-
-    { enum: [   // You should write one of this dish type into dish type property "dishType: "main_course",.
-        "main_course",
-        "pasta",
-        "pizza",
-        "soup",
-        "snack",
-        "drink",
-        "dessert",
-        "other",
-      ],
-    },
-    image:"/images/dishes/bean.jpg" // This is valid for dishes
-    image:"/images/drinks/rode-wine.jpg" // This is valid for drinks
-
+{
+    const dishes = [
     
-}, {
+    
+        {
+            title: "Mushroom Soup",
 
-    title: "Apple tart",//example 2
+            ingredients: ["Musroom", "Onion", "Salt", "Garlic", "Flour"],
 
-    ingredients: ["Apple", "Sugar", "Flour", "Cinnamon"], //example
+            cuisine: "Turkish",
+            dishType: "soup",
+            price: 12.5,
+            image: "/images/soups/mushroom_soup.jpg"
+       
+        },
+       
+       {
+            title: "Pumpkin Soup",
 
-    cuisine: "Turkish",
-    dishType: "dessert",
+            ingredients: ["Pumpkin", "Onion", "Salt", "Potatoes", "Garlic", "Fresh Parsley"],
+
+            cuisine: "Haiti",
+            dishType: "soup",
+            price: 12.5,
+             image:"/images/soups/pumpkin_soup.jpg",
+
+        }
+        ,
+      
+        {
+            title: "Tomato Soup",
+
+            ingredients: ["Butter", "Onion", "Sugar", "Whipping Cream", "Garlic", "Tomatoes", "Parmesan Cheese"],
+
+            cuisine: "Spain",
+            dishType: "soup",
+            price: 12.50,
+            image: "/images/soups/tomato_soup.jpg",
+        },
+        {
+            title: "Vegetable Soup",
+
+            ingredients: ["Butter", "Onion", "Sugar", "Whipping Cream", "Garlic", "Tomatoes", "Parmesan Cheese"],
+
+            cuisine: "Spain",
+            dishType: "soup",
+            price: 12.50,
+            image: "/images/soups/vegatable_soup.jpg"
+         
+        },
+    
+    
+    
+        {
+            title: "Chicken Katsu Hero",
+            ingredients: ["Chicken", "Onion", "Pickles", "Mayonnaise", "Egg","Olive Oil"],
+            cuisine: "Japanese",
+            dishType: ["main_course"],
+            price: 22.75,
+            image: "/images/maincourses/chicken_katsu_hero.jpg"
+       
+        },
+    
+        {
+            title: "Lamb Chops Sizzled with Garlic",
+            ingredients: ["lamp", "Onion", "Pepper", "Lemon Juice", "Parsley","Olive Oil","Red Pepper"],
+            cuisine: "Spain",
+            dishType: "main_course",
+            price: 24.50,
+            image: "/images/maincourses/Lamb-Chops-Sizzled-with-Garlic.jpg"
+    
+
+        },
+    {
+            title: "Steak Horizo Potato Kabobs",
+            ingredients: ["Chorizo slices", "Onion", "Potatoes", "Olive Oil", "Smoked Paprika","Red Wine Vinegar"],
+            cuisine: "France",
+            dishType: "main_course",
+            price: 23.50,
+            image: "/images/maincourses/steak_chorizo_potato_kabobs.jpg"
+    
+
+        }
+         ,
+
+            {title: "Steak Au Poivre Red Wine Pan Sauce Hero",
+            ingredients: ["Rib Eye Steak", "Black Pepper", "Butter", "Minced Shallots", "Red Wine","Beef Broth"],
+            cuisine: "France",
+            dishType: "main_course",
+            price: 22.50,
+            image:  "/images/maincourses/steak-au-poivre-red-wine-pan-sauce-hero"
+    
+
+        },
+    
+        {
+            title: "Fettucine",
+            ingredients: ["Fettuccine pasta", "Butter", "Havy cream", "Garlic Salt", "Romano Cheese", "Parmesan Cheese"],
+            cuisine: "Italy",
+            dishType: "pasta",
+            price: 19.50,
+            image: "/images/pasta_photos/Fettucine.jpg"
+          
+        },
+    
+          { title: "Macoroni",
+            ingredients: ["Elbow macaroni", "Butter", "Flour", "Salt", "Black Pepper","Cheddar Cheese","Milk"],
+            cuisine: "Italy",
+            dishType: "pasta",
+            price: 19.50,
+            image: "/images/pasta_photos/macaroni.jpg"
+    
+
+        },
+    
+           { title: "Mozzarella Penne Rosa",
+            ingredients: ["Dried Tomatoes", "Garlic", "Mushrooms", "Simmer Marina Sauce", "Mozzarella Cheese","Spinach","Basil"],
+            cuisine: "Italy",
+            dishType: "pasta",
+            price: 19.50,
+            image: "/images/pasta_photos/ Mozzarella_Penne_Rosa.jpg"
+    
+
+        },
 
 
-    { enum: [   // You should write one of this dish type into dish type property "dishType: "main_course",.
-        "main_course",
-        "pasta",
-        "pizza",
-        "soup",
-        "snack",
-        "drink",
-        "dessert",
-        "other",
-      ],
-    },
-    image:"/images/desserts/apple-tart.jpg" // This is valid for 
-   
-}]
+          { title: "Ravioli",
+            ingredients: ["Flour", "Sea Salt", "Freshly Grated Nutmeg", "Eggs", "Virgin Olive Oil"],
+            cuisine: "Italy",
+            dishType: "pasta",
+            price: 19.50,
+            image: "/images/pasta_photos/ Ravioli.jpg"
+    
+
+        },
+ 
+          { title: "Neapolitan ",
+            ingredients: ["Tomato sauce", "Mozzzarella cheese", "Semolina Flour"],
+            cuisine: "Italy",
+            dishType: "pizza",
+            price: 17.00,
+            image: "/images/pizza_photos/ Neapolitan-pizza.jpg"
+    
+
+        },
+         
+          {
+            title: "Pepperoni",
+            ingredients: ["Tomato Paste", "Dried Oregano", "Dried Basil","Garlic Powder","Onion Powder","Black Pepper","Sugar","Salt"],
+            cuisine: "Italy",
+            dishType: "pizza",
+            price: 17.00,
+            image: "/images/pizza_photos/Pepperoni_pizza.jpg"
+    
+
+        },
+    
+
+           {
+            title: "Margarita",
+            ingredients: ["Tomato Paste", "Mozzarella Balls", "Olive Oil","Salt"],
+            cuisine: "Italy",
+            dishType: "pizza",
+            price: 17.00,
+            image: "/images/pizza_photos/ pizza_margarita.jpg"
+    
+
+        },
+
+             {
+            title: "Romana",
+            ingredients: ["Dry Yeast", "Romana Flour", "Water","Salt"],
+            cuisine: "Italy",
+            dishType: "pizza",
+            price: 17.00,
+            image: "/images/pizza-photos/pizza_romana.jpg"
+    
+
+        }
+       ,
+             {
+            title: "Manti",
+            ingredients: ["Egg", "Wheat Flour", "Water","Salt","Beef","Yoghurt","Garlic","Red Pepper"],
+            cuisine: "Turkiye",
+            dishType: "snack",
+            price: 19.00,
+            image: "/images/snack_photos/manti.jpg"
+    
+
+        },
+
+{
+            title: "Sarma",
+            ingredients: ["Pickled Wine Leaves", "Rice", "Parsley","Fresh Mint","Onion","Olive Oil","Black Pepper"],
+            cuisine: "Turkiye",
+            dishType: "snack",
+            price: 19.00,
+            image: "/images/snack_photos/sarma.jpg"
+    
+
+        }
+
+        ,
+{
+            title: "Bluberry Pie",
+            ingredients: [" Blueberries", "Ground Cinnamon", "Sugar","Cornstarch","Pie Crust","Granulated Sugar","Lemon Juice","Butter"," Fresh Blueberries"],
+            cuisine: "The United Kingdom",
+            dishType: "dessert",
+            price: 19.00,
+            image: "/images/dessert_photos/Blueberry-pie.jpeg"
+    
+
+        },
+
+{
+            title: "Chocolate souffle",
+            ingredients: ["Butter", "Sugar", "Milk","Dark Chocolate","Cocoa Powder","Egg","Vanilla Extract"],
+            cuisine: "France",
+            dishType: "dessert",
+            price: 19.00,
+            image: "/images/dessert_photos/chocolate-souffle.jpg"
+    
+
+        },
+        {
+            title: "Tiramisu",
+            ingredients: ["Cocoa Powder", "Sugar", "Ladyfingers","Egg yolks","Coffe","Egg","Vanilla Extract","Mascarpone"],
+            cuisine: "Italy",
+            dishType: "dessert",
+            price: 19.00,
+            image: "/images/dessert_photos/tiramisu.jpg"
+    
+
+        }
+        ,
+        {
+            title: "Cabernet Sauvignon",
+            ingredients: "Red Grapes",
+            cuisine: "France",
+            dishType: "drink",
+            price: 24.00,
+            image: "/images/drinks_photos/Cabernet-Sauvignon.jpeg"
+    
+
+        },
+{
+           title: "Rose Sparkling",
+            ingredients: "Red Grapes,Sugar,Yeast",
+            cuisine: "Italy",
+            dishType: "drink",
+            price: 24.00,
+            image: "/images/drinks_photos/Rose_Sparkling_Wine.jpeg"
+    
+
+        },
+
+{
+            title: "Coca Cola",
+            ingredients: "Null",
+            cuisine: "Atlanta/Georgia",
+            dishType: "drink",
+            price: 7,
+            image: "/images/drinks_photos/coca-cola-original-12oz.jpg"
+    
+
+        },
+        
+        {
+            title: "Sparkling Water",
+            ingredients: "Null",
+            cuisine: "England",
+            dishType: "drink",
+            price: 4,
+            image: "/images/drinks_photos/Sparkling_Water.jpg"
+    
+
+        },
+
+
+
+
+
+
+
+
+
+
+
+
+    ]
+}
