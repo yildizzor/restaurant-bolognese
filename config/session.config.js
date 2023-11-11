8d7d62aa370f67971eb78b0420a05ad31493c720
Yildiz Zor Unlu <yildizzor@gmail.com>
2023-11-11 14:55:40 +0100

    some changes are done

--- config/session.config.js
+++ config/session.config.js
@@ -25,9 +25,12 @@ module.exports = (app) => {
 
   app.use(function (req, res, next) {
     // Make `user` and `authenticated` available in templates
-    res.locals.isLoggedIn = req.session.currentUser !== undefined
-   
-    next()
-  })
-  
+    res.locals.isLoggedIn = true;
+    res.locals.isLoggedOut = true;
+
+    // res.locals.isLoggedIn = req.session.currentUser !== undefined
+    // res.locals.isLoggedOut = req.session.currentUser === undefined
+
+    next();
+  });
 };
